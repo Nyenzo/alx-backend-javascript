0625ba6a93135ae347d44fdb875fd2fd7980e9de
Nyenzo <nyenzoisabwa@gmail.com>
2024-03-04 15:15:50 +0300

    ES6 basics

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,11 +1,7 @@
 export default function createIteratorObject(report) {
-  return {
-    * [Symbol.iterator]() {
-      for (const department of Object.values(report.allEmployees)) {
-        for (const employee of department) {
-          yield employee;
-        }
-      }
-    },
-  };
+  const result = [];
+  for (const idx of Object.values(report.allEmployees)) {
+    result.push(...idx);
+  }
+  return result;
 }
--- 0x00-ES6_basic/4-rest-parameter.js
+++ 0x00-ES6_basic/4-rest-parameter.js
@@ -1,9 +1,3 @@
 export default function returnHowManyArguments(...theArgs) {
-  let counter = 0;
-  for (const x of theArgs) {
-    if (x) {
-      counter += 1;
-    }
-  }
-  return counter;
+  return theArgs.length;
 }
