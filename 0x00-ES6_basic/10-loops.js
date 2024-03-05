195e009b0d45338361ebeec8618d9d8e36139d8a
Nyenzo <nyenzoisabwa@gmail.com>
2024-03-05 14:53:53 +0300

    ES6 basics

--- 0x00-ES6_basic/10-loops.js
+++
@@ -1,8 +0,0 @@
-export default function appendToEachArrayValue(array, appendString) {
-  const result = [];
-  for (const element of array) {
-    result.push(appendString + element);
-  }
-
-  return result;
-}
--- 0x00-ES6_basic/README.md
+++
@@ -1,1 +0,0 @@
-This is a repository for es6 concepts in javascript
