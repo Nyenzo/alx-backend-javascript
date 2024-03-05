9cbb96fb2b2c9fe4217c76bc00dac61152ee02e4
Nyenzo <nyenzoisabwa@gmail.com>
2024-03-05 14:43:42 +0300

    ES6 basics

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,8 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
-  const newArray = [];
-  for (const value of array) {
-    newArray.push(appendString + value);
+  const result = [];
+  for (const element of array) {
+    result.push(appendString + element);
   }
 
-  return newArray;
+  return result;
 }
--- 0x00-ES6_basic/4-rest-parameter.js
+++ 0x00-ES6_basic/4-rest-parameter.js
@@ -1,9 +1,3 @@
-export default function returnHowManyArguments(...theArgs) {
-  let counter = 0;
-  for (const x of theArgs) {
-    if (x) {
-      counter += 1;
-    }
-  }
-  return counter;
+export default function returnHowManyArguments(...args) {
+  return args.length;
 }
